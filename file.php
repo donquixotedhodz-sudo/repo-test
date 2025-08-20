54b2ac6edbdf107aebc6118e5c92832e1d2117ed
donquixotedhodz-sudo <donquixotedhodz@gmail.com>
2025-08-20 14:56:53 -0700

    josh/repo

---
+++ file.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
+<?php
+echo "My first PHP script!";
+echo "Si pidang ay puro tanong, tas si AJ tahimik lang.";
+echo "ayayayayayayayayayayayayyyy";
+?>
+
+</body>
+</html>
